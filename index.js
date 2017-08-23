beb6bca4c745a246a5ee732ee702eadc365956b7
myf <myf@worldtalks.org>
2017-08-22 23:26:39 -0400

    group echo

--- index.js
+++ index.js
@@ -2,19 +2,30 @@ const dbus = require('dbus');
 
 const bus = dbus.getBus('session');
 
-bus.getInterface(
+let get_interface = (cb) => bus.getInterface(
     'org.asamk.Signal',
     '/org/asamk/Signal',
     'org.asamk.Signal', (err, inter) => {
-     
-        if (err) console.log(err);
-        inter.on('MessageReceived', (timestamp,
-            sender,
-            groupid,
-            message,
-            attachments) => {
+        cb(inter);
+    }
+);
+
+
+get_interface((inter) => {
+    inter.on('MessageReceived', (timestamp,
+        sender,
+        groupid,
+        message,
+        attachments) => {
+            debugger;
+            if (groupid) {
+                inter.sendGroupMessage(message, attachments, groupid, function(err) {
+                    if(err) console.log(err);
+                });
+            } else {
                 inter.sendMessage(message, attachments, [sender], function(err) {
                     if(err) console.log(err);
                 });
-        });
+            }
+    });
 });
